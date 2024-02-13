3dada4898490cbedbbb406dc32501fcd67fad169
Evgeniy Gerbin <zenagerbin@gmail.com>
2024-02-13 12:14:13 +0200

    added func custom map

---
+++ map.py
@@ -0,0 +1,14 @@
+def summa(num):
+    return num + num
+
+listik = [1,4,5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9, 9, 9]
+
+def my_map(func, iterable):
+    my_list = []
+    for item in iterable:
+        res = func(item)
+        my_list.append(res)
+    return print(my_list)
+
+my_map(summa, listik)
+
